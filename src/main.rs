c215b4de5c042d202d4ce1bd150ad99815fc70df
Correia Alves Cedric <cedric.correia.alves@gmail.com>
2023-04-02 17:08:05 +0200

    added clap to have proper cli parsing

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,37 @@ extern crate dotenv_codegen;
 
 use std::collections::HashMap;
 
+use clap::{Args, Parser, Subcommand};
 use serde::{Deserialize, Serialize};
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Hue(HueArgs),
+}
+
+#[derive(Args)]
+struct HueArgs {
+    #[command(subcommand)]
+    command: HueCommands,
+}
+
+#[derive(Subcommand)]
+enum HueCommands {
+    List,
+    On {
+        group: String,
+        color: Option<String>,
+    },
+    Off {
+        group: String,
+    },
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Group {
@@ -63,7 +93,20 @@ async fn get_hue() -> Result<HashMap<String, Group>, Box<dyn std::error::Error>>
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = get_hue().await?;
-    println!("{:#?}", resp);
+    let cli = Cli::parse();
+    match cli.command {
+        Commands::Hue(hue) => {
+            let hue_cmd = hue.command;
+            match hue_cmd {
+                HueCommands::List {} => {
+                    let resp = get_hue().await?;
+                    println!("{:#?}", resp);
+                }
+                _ => {
+                    println!("need to be implemented")
+                }
+            }
+        }
+    }
     Ok(())
 }
