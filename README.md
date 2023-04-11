7c28b125eae36a75401ed905a39db18cbcb22023
Correia Alves Cedric <cedric.correia.alves@gmail.com>
2023-04-11 21:41:57 +0200

    added support for razer devices at the moment only fixed static color works

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # led-controll
 
 a small rust app to controll all my leds
+
+
+## Philips Hue
+
+philips hue api [official doc](https://developers.meethue.com/develop/get-started-2/), [unofficial doc](https://www.burgestrand.se/hue-api/)
+
+## Razer
+
+for razer devices the [openrazer drivers](https://openrazer.github.io/) are required and udev is used to check for the devices
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 #[macro_use]
 extern crate dotenv_codegen;
 
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    ffi::{CString, OsStr},
+    fs,
+    io::Write,
+    os::unix::prelude::OsStrExt,
+};
 
 use clap::{Args, Parser, Subcommand};
 use serde::{Deserialize, Serialize};
@@ -15,6 +21,7 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     Hue(HueArgs),
+    Razer,
 }
 
 #[derive(Args)]
@@ -95,7 +102,7 @@ async fn get_hue() -> Result<HashMap<String, Group>, Box<dyn std::error::Error>>
     return Ok(resp);
 }
 
-async fn toggle_hue_group(group: String, state: bool) -> Result<(), Box<dyn std::error::Error>> {
+async fn change_hue_group(group: String, state: bool) -> Result<(), Box<dyn std::error::Error>> {
     let mut map = HashMap::new();
     map.insert("on", state);
     reqwest::Client::builder()
@@ -121,13 +128,37 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     println!("{:#?}", resp);
                 }
                 HueCommands::On { group, color } => {
-                    toggle_hue_group(group, true).await?;
+                    change_hue_group(group, true).await?;
                 }
                 HueCommands::Off { group } => {
-                    toggle_hue_group(group, false).await?;
+                    change_hue_group(group, false).await?;
                 }
                 _ => {
                     println!("need to be implemented")
+                }
+            }
+        }
+        Commands::Razer {} => {
+            let context = libudev::Context::new().unwrap();
+            let mut enumerator = libudev::Enumerator::new(&context).unwrap();
+
+            enumerator.match_subsystem("hid").unwrap();
+
+            for device in enumerator.scan_devices().unwrap() {
+                if let Some(x) = device.attribute_value("device_type") {
+                    let syspath = device.syspath().unwrap();
+                    println!("{:?},{:?}", device.sysname(), syspath);
+                    println!("attributes");
+                    for attribute in device.attributes() {
+                        println!("attr {:?} = {:?}", attribute.name(), attribute.value());
+                    }
+                    let buf = &[0x00u8, 0xFFu8, 0x00u8];
+                    println!("WE GOT A DEVICE: {:?}", x);
+
+                    let static_path = syspath.join("logo_matrix_effect_static");
+                    let mut file = fs::File::create(static_path)?;
+                    file.write(buf)?;
+                    // device.set_attribute_value("logo_matrix_effect_static", "\x0f\x00\x00")?;
                 }
             }
         }
